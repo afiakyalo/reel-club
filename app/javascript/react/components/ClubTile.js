7043e77e1d7eedaabc5f93d0062fb507230acf82
Afia Kyalo <akyalo@bdtrust.org>
2020-02-13 13:28:37 -0500

    Add tests

--- app/javascript/react/components/ClubTile.js
+++ app/javascript/react/components/ClubTile.js
@@ -4,9 +4,9 @@ const ClubTile = (props) => {
   const { name, description, genre } = props.clubInfo
   return(
     <div className="callout">
-      <h3>{name}</h3>
-      <p>{description}</p>
-      <p>Main Genre: {genre}</p>
+      <h3 id="name">{name}</h3>
+      <p id="description">{description}</p>
+      <p id="genre">Main Genre: {genre}</p>
       <div>
         <a href="" className="button">Join</a>
       </div>
---
+++ app/javascript/react/components/ClubTile.test.js
@@ -0,0 +1,40 @@
+import React from "react"
+import Enzyme, { mount } from "enzyme"
+import Adapter from "enzyme-adapter-react-16"
+import { BrowserRouter } from "react-router-dom"
+
+Enzyme.configure({ adapter: new Adapter() })
+
+import ClubTile from "./ClubTile"
+
+describe("ClubTile", () => {
+  let wrapper
+  let clubInfo
+
+  beforeEach(() => {
+    clubInfo = {
+      name: "Jailbirds",
+      description: "We watch thrillers and comedy",
+      genre: "Thriller"
+    }
+    wrapper = mount(
+      <BrowserRouter>
+        <ClubTile
+          clubInfo={clubInfo}
+        />
+      </BrowserRouter>
+    )
+  })
+
+  it("should render a h3 element with the name of the club", () => {
+    expect(wrapper.find("#name").text()).toBe("Jailbirds")
+  })
+
+  it("should render a p element with the description of the club", () => {
+    expect(wrapper.find("#description").text()).toBe("We watch thrillers and comedy")
+  })
+
+  it("should render a p element with the main genre of the club", () => {
+    expect(wrapper.find("#genre").text("#genre")).toBe("Main Genre: Thriller")
+  })
+})
