a3ae02d744c88a3b848caf0ab16dbe99c4914d18
Afia Kyalo <akyalo@bdtrust.org>
2020-02-17 13:36:09 -0500

    fix syntax

--- app/javascript/react/components/ClubTile.js
+++ app/javascript/react/components/ClubTile.js
@@ -8,7 +8,7 @@ const ClubTile = (props) => {
       <h3 id="name">{name}</h3>
       <p id="description">{description}</p>
       <p id="genre">Main Genre: {genre}</p>
-      <div class="sticky">
+      <div>
         <a href="" className="button">Join</a>
       </div>
     </div>
