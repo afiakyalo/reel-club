4c20ac0e813dc2984c06e5387b70842ae981f3cb
Afia Kyalo <akyalo@bdtrust.org>
2020-02-26 10:29:09 -0500

    added default image

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,6 @@ class ApplicationController < ActionController::Base
   protected
 
    def configure_permitted_parameters
-     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :city, :state, :description])
+     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :city, :state, :description, :avatar])
    end
 end
