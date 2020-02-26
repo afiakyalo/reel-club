4625199912810a88fa2c2c20e5180fed47130de9
Afia Kyalo <akyalo@bdtrust.org>
2020-02-26 12:49:30 -0500

    fixed bug when missing movie

--- app/javascript/react/components/MovieTile.js
+++ app/javascript/react/components/MovieTile.js
@@ -4,12 +4,16 @@ const MovieTile = (props) => {
   const { movie, errors } = props
   let title, synopsis, release_date, rating, image;
 
-  if(movie) {
+  if (movie) {
     title = movie.title,
     synopsis = movie.synopsis,
-    release_date = movie.release_date
-    rating = movie.rating
-    image = movie.poster
+    release_date = movie.release_date,
+    rating = movie.rating,
+    image = `http://image.tmdb.org/t/p/w185/${movie.poster}`
+  } else {
+    image = `https://reel-club-development.s3.amazonaws.com/uploads/poster/default_poster.png`,
+    title = "No movie has been selected",
+    synopsis = "The club owner should search for a title below."
   }
 
   const handleClick = (event) => {
@@ -21,7 +25,7 @@ const MovieTile = (props) => {
     <>
       <h5>Movie of the Week</h5>
       <h5>{errors}</h5>
-      <img className="poster" src={`http://image.tmdb.org/t/p/w185/${image}`}/>
+      <img className="poster" src={image}/>
       <div>
         <span id="title">{title}</span>
         <p id="synopsis">{synopsis}</p>
